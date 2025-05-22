140ad9f68d1d1bda8f1e10b2dc5066c423f8fb60
Bar Yosef Tayar Mac <bartayar123456789@gmail.com>
2025-05-22 13:18:58 +0300

    register and update

--- src/routes/hotels.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as HotelsController from '../controllers/hotels.controller.js'
-import { verifyToken } from '../middlewares/auth.js';
-const router = express.Router();
-
-router.get('/getHotels',verifyToken, HotelsController.getHotels);
-
-
-export default router;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,20 +1,19 @@
 import express from 'express';
 import * as UserControllers from '../controllers/user.controller.js';
 import { verifyToken } from '../middlewares/auth.js';
-import * as HotelsController from '../controllers/hotels.controller.js'
 
 const router = express.Router();
 
-
+router.get('/getUser', verifyToken, UserControllers.getUser);
 router.post('/register', UserControllers.register);
 router.post('/login', UserControllers.login);
-router.put('/:userId',verifyToken, UserControllers.updateUser);
+router.put('/updateUser', verifyToken, UserControllers.updateUser);
 router.delete('/:userId', UserControllers.deleteUser);
 router.get('/location', verifyToken, UserControllers.getUserLocation);
 router.get('/usersLocations', UserControllers.getUserLocations);
 router.get('/auth/check', verifyToken, (req, res) => {
     res.status(200).json({ userId: req.user.id });
 });
-  
+
 
 export default router;
