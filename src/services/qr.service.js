04576daf95595cb6aecf7db2f7ac766885b2e5c8
Bar Yosef Tayar <135449270+BarYosef212@users.noreply.github.com>
2025-06-21 21:03:17 +0300

    # (#39)

--- src/services/qr.service.js
+++ src/services/qr.service.js
@@ -1,7 +1,7 @@
 import QRCode from 'qrcode';
 
 export const generateUserQRCode = async (userId) => {
-  const url = `${process.env.FRONTEND_URL}/users/${userId}`;
+  const url = `${process.env.FRONTEND_URL}/profile/${userId}`;
   try {
     const qrDataURL = await QRCode.toDataURL(url);
     return qrDataURL;
